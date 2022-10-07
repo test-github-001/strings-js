0613f7df13c370d1e8ed230890a53b74f16d16ed
test-github-001 <107862407+test-github-001@users.noreply.github.com>
2022-10-07 09:53:57 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/* Developer:
+Algo Ritm */
+
+const developer = 'Algo Ritm';
+const language = 'JavaScript';
+
+let feature = 'best';
+
+console.log('Hi, my name is ' + developer + '!');
+console.log(`${language} - is the ${feature} programming language!`);
+
+// Bonus tasks
+let year = 1997;
+console.log(language + ' appeared in ' + year);
+
+feature = 'amazing';
+console.log(language, '- is the ', feature, ' programming language!');
